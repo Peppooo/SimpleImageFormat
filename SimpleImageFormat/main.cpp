8b7fefce9c8ed8640497fbb5d33a2bfebfc546c8
Peppooo <pietrofacchini46@gmail.com>
2022-11-10 22:45:43 +0100

    Added a painting method for creating sif images

--- SimpleImageFormat/main.cpp
+++ SimpleImageFormat/main.cpp
@@ -33,7 +33,60 @@ struct rgba {
 
 vector2D res;
 
-int main(int argc, char* argv[])
+int paint()
+{
+    res.set(1920, 1080);
+    cout << "file name: ";
+    fstream image;
+    {string input; cin >> input; image.open(input, ios_base::app); }
+    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        SDL_Window* window = NULL;
+        SDL_Renderer* renderer = NULL;
+
+        if (SDL_CreateWindowAndRenderer(res.x, res.y, 0, &window, &renderer) == 0) {
+            SDL_bool done = SDL_FALSE;
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+            while (!done) {
+                SDL_Event event;
+                int mousex, mousey;
+                Uint32 buttons;
+
+                SDL_PumpEvents();
+
+                buttons = SDL_GetMouseState(&mousex, &mousey);
+
+                int previous_pos[2];
+                if ((buttons & SDL_BUTTON_LMASK) != 0) {
+                    SDL_RenderDrawPoint(renderer, mousex, mousey);
+                    SDL_RenderPresent(renderer);
+                    if (previous_pos[0] != mousex and previous_pos[1] != mousey) {
+                        image << "0;0;0;1;" << mousex << ";" << mousey << endl;
+                    }
+                    previous_pos[0] = mousex; previous_pos[1] = mousey;
+                }
+
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) {
+                        done = SDL_TRUE;
+                    }
+                }
+            }
+
+            if (renderer) {
+                SDL_DestroyRenderer(renderer);
+            }
+            if (window) {
+                SDL_DestroyWindow(window);
+            }
+        }
+        SDL_Quit();
+        return 0;
+    }
+}
+
+int reader()
 {
     res.set(1920, 1080);
     cout << "file name: ";
@@ -63,54 +116,11 @@ int main(int argc, char* argv[])
                         token = line.substr(0, line.find_first_of(";"));
                         line = line.substr(line.find_first_of(";") + 1);
                         data[i] = stoi(token.c_str());
-                        //cout << "number " << i  << " color " << color[i] << endl;
                         i++;
                     }
-                    cout << "drawing pixel at " << data[4] << "x" << data[5] << " with color " << Uint8(data[1] & 0xFF) << Uint8(data[2] & 0xFF) << Uint8(data[3] & 0xFF);
                     SDL_SetRenderDrawColor(renderer, Uint8(data[0] & 0xFF), Uint8(data[1] & 0xFF), Uint8(data[2] & 0xFF), Uint8(data[3] & 0xFF));
                     SDL_RenderDrawPoint(renderer, data[4], data[5]);
                     SDL_RenderPresent(renderer);
-                    //cout << "new pixel" << endl;
-                    // finito il pixel
-                }
-            }
-
-            while (!done) {
-                
-                SDL_Event event;
-                int mousex, mousey;
-                Uint32 buttons;
-
-                SDL_PumpEvents();  // make sure we have the latest mouse state.
-
-                buttons = SDL_GetMouseState(&mousex, &mousey);
-
-                SDL_Log("Mouse cursor is at %d, %d", mousex, mousey);
-                if ((buttons & SDL_BUTTON_LMASK) != 0) {
-                    SDL_Log("Mouse Button 1 (left) is pressed.");
-                }
-                /*
-
-                int y = 0;
-                for (int x = 0; x < pixels.size(); x++) {
-                    if (x == res.x + 1) {
-                        y++;
-                        x = 0;
-                    }
-                    SDL_SetRenderDrawColor(renderer, pixels[x].r, pixels[x].g, pixels[x].b, pixels[x].a);
-                    SDL_RenderDrawPoint(renderer, x, y);
-                }
-                for (int y = 0; y < res.y; y++) {
-                    for (int x = 0; x < res.x; x++) {
-                        SDL_RenderDrawPoint(renderer, x, y);
-                    }
-                }
-                SDL_RenderPresent(renderer);
-                */
-                while (SDL_PollEvent(&event)) {
-                    if (event.type == SDL_QUIT) {
-                        done = SDL_TRUE;
-                    }
                 }
             }
         }
@@ -122,6 +132,24 @@ int main(int argc, char* argv[])
             SDL_DestroyWindow(window);
         }
     }
-    SDL_Quit();
+    cout << "ibbra";
+    {string a;cin >> a;}
     return 0;
 } 
+
+int main(int argc, char* argv[]) {
+    cout << "\tSimple Image Format (sif)\n(1) Open image creator\n(2) Open image viewer\n";
+    {
+        int input; cin >> input;
+        if (input == 1) {
+            paint();
+        }
+        else if (input == 2) {
+            reader();
+        }
+        else {
+            cout << "invalid argument";
+            exit(1);
+        }
+    }
+}
